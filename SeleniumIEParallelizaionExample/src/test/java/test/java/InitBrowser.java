beaf924bf2180ecc6451474d3546f15bc5b7c5df
RBERZINS <RBERZINS@RBERZINS.qube.local>
2016-04-22 16:46:42 +0300

    Refactoring

--- SeleniumIEParallelizaionExample/src/test/java/test/java/InitBrowser.java
+++ SeleniumIEParallelizaionExample/src/test/java/test/java/InitBrowser.java
@@ -14,37 +14,14 @@ public class InitBrowser {
 	
 	public WebDriver loadDriver(String browserVersion) throws Exception{
 		DesiredCapabilities capab = new DesiredCapabilities();
-		
-		switch(browserVersion){
-			case "9":
-				System.setProperty("webdriver.ie.driver", "C:\\selenium\\IEDriverServer.exe");
-				threadDriver = new ThreadLocal<RemoteWebDriver>();
-				capab = DesiredCapabilities.internetExplorer();
-				capab.setBrowserName("internet explorer");
-				capab.setVersion("9");
-				capab.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-				threadDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capab));
-				break;
-			case "10":
-				System.setProperty("webdriver.ie.driver", "C:\\selenium\\IEDriverServer.exe");
-				threadDriver = new ThreadLocal<RemoteWebDriver>();
-				capab = DesiredCapabilities.internetExplorer();
-				capab.setBrowserName("internet explorer");
-				capab.setVersion("10");
-				capab.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-				threadDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capab));
-				break;
-			case "11":
-				System.setProperty("webdriver.ie.driver", "C:\\selenium\\IEDriverServer.exe");
-				threadDriver = new ThreadLocal<RemoteWebDriver>();
-				capab = DesiredCapabilities.internetExplorer();
-				capab.setBrowserName("internet explorer");
-				capab.setVersion("11");
-				capab.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-				threadDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capab));
-				break;
-		}
-		
+
+		System.setProperty("webdriver.ie.driver", "C:\\selenium\\IEDriverServer.exe");
+		threadDriver = new ThreadLocal<RemoteWebDriver>();
+		capab = DesiredCapabilities.internetExplorer();
+		capab.setBrowserName("internet explorer");
+		capab.setVersion(browserVersion);
+		capab.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		threadDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capab));
 		threadDriver.get().manage().window().maximize();
 		threadDriver.get().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		threadDriver.get().manage().deleteAllCookies();
